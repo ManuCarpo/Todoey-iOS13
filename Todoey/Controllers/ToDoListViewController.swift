883304d85400b60d51f1fd3485f7237c63ab2031
Emanuele Carpigna <emanuelecarpigna@gmail.com>
2020-10-18 02:39:34 +0200

    Creata funzione che aggiunge alla view l'itemArray: i nuovi elementi creati dall'utente, cioè gli elementi salvati nel Core Data Aggiunta la searchBar Aggiunte alcune funzionalità per la SearchBar: posso cercare lettere/parole che sono contenute nell'array. Visualizzo nell'app solo le celle che contengono quelle lettere/parole e una volta cliccata la x nella searchBar, vengono di nuovo visualizzate tutte le celle. Inoltre cessa di essere vista la linea nella searchBar e scompare la keyboard -- Create le varie fasi del Core Data Aggiunte le funzioni che permettono all'app di salvare i dati, mantenere i dati post chiusura e ricaricarli nella view.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
     
-    
     var itemArray = [Item]()
-    //Creato il Path che mi serve per conoscere dove andare a salvare gli elementi dell'applicazione aggiunti dall'utente.
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //Creo un oggetto (copia del BluePrint della classe AppDelegate).proprietà
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Permetto all'utente di visualizzare i cambiamenti che ha fatto negli utilizzi precedenti, l'array salvato nel defaults viene aggiornato nella parte "addButton: New Items"
-        loadItems()
+            loadItems()
         
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     }
     //MARK: -  TableView
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,7 +41,10 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Cliccando nella cella, rendo il checkmark visibile o non visibile, all'opposto di ciò che visualizzo nella cella prima di cliccare
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+       itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+
+       
         
         saveItems()
         
@@ -50,7 +54,6 @@ class ToDoListViewController: UITableViewController {
     //MARK: -  addButton: New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
         var textField = UITextField()
         
         //Creato un allert con titolo, messaggio e stile
@@ -59,14 +62,13 @@ class ToDoListViewController: UITableViewController {
         // Creata un' azione che triggera quando l'user clicca il pulsante "Add Item"
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
             // Viene aggiunto ciò che scrive l'utente all'array iniziale
             self.itemArray.append(newItem)
             
             self.saveItems()
-            
         }
         
         // Ho dato all'allert un text field che per permettere all'utente di scriverci
@@ -86,15 +88,13 @@ class ToDoListViewController: UITableViewController {
     
     //Creata funzione che salva nella P.List gli elementi creati dall'utente
     func saveItems() {
-        // Create an object that encodes instances of data types to a property list.
-        let encoder = PropertyListEncoder()
+        
         
         //Blocco do-catch per controllare eventuali errori quando aggiungo alla property list nuovi elementi
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch  {
-            print("Error encoding item array, \(error)")
+            print("Errorsaving context \(error)")
         }
         
         // Viene aggiornato il Database per permettere alla view di far vedere i nuovi elementi aggiunti nell'array
@@ -102,16 +102,65 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //Creata funzione che aggiunge alla view l'itemArray: i nuovi elementi creati dall'utente, cioè gli elementi salvati nel NSDecoder
+    //    Creata funzione che aggiunge alla view l'itemArray: i nuovi elementi creati dall'utente, cioè gli elementi salvati nel Core Data
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print(error)
-            }
+        //Creo una costante che recupera dati attraverso il criterio "NSFetchRequest<Item>" (usato per recuperare data dal persistent store)
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            // Return an array of objects that meet the criteria specified by a given fetch request. The given fetch request is "request". Salvo i dati uguagliando la funzione all'itemArray.
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context\(error)")
         }
+        
     }
 }
 
+//  Qui sotto il metodo per eliminare dati dal Core Data
+//  context.delete(itemArray[indexPath.row])
+//  itemArray.remove(at: indexPath.row)
+//  saveItems()
+
+
+//MARK: -  Search Bar
+
+//Nella SearchBar posso cercare lettere/parole che sono contenute nell'array. Visualizzo nell'app solo le celle che contengono quelle lettere/parole
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context\(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+    //Una volta cliccata la x nella searchBar, vengono dinuovo visualizzate tutte le celle. Inoltre cessa di essere vista la linea nella searchBar e scompare la keyboard
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            let request: NSFetchRequest<Item> = Item.fetchRequest()
+            do {
+                itemArray = try context.fetch(request)
+            } catch {
+                print("Error fetching data from context\(error)")
+            }
+            tableView.reloadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+        
+        
+    }
+}
